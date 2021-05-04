31fb057100abc74b10816f67a6f387320ae7a294
Léo ROUILLARD <leorouillard@gmail.com>
2021-05-04 14:44:18 +0200

    création modèle/factory/seeder/controller organisation

---
+++ database/migrations/2021_05_04_122754_create_organisations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrganisationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('organisations', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug')->unique();
+            $table->string('name');
+            $table->string('email')->nullable();
+            $table->string('tel')->nullable();
+            $table->text('address');
+            $table->enum('type', ['school', 'client', 'government']);
+            $table->date('created_at');
+            $table->date('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('organisations');
+    }
+}
