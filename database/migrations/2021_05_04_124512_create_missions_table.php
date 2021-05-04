16e70ae76246da65f0ceba3fd3b4deaaf54aa140
Léo ROUILLARD <leorouillard@gmail.com>
2021-05-04 14:48:31 +0200

    création modèle/factory/seeder/controller mission

---
+++ database/migrations/2021_05_04_124512_create_missions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('missions', function (Blueprint $table) {
+            $table->id();
+            $table->string('reference');
+            $table->integer('organisation_id')->unsigned();
+            $table->foreign('organisation_id')->references('id')->on('organisations');
+            $table->string('title');
+            $table->string('comment')->nullable();
+            $table->integer('deposit');
+            $table->date('ended_at');
+            $table->date('created_at');
+            $table->date('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('missions');
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\Organisation::factory()->create();
+        //\App\Models\Mission::factory()->create();
     }
 }
